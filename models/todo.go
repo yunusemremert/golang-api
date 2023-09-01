ffee28c7777d85085d113ad9940da35a8737984a
Yunus Emre MERT <yunusemremert@yandex.com>
2023-09-01 15:44:07 +0300

    hotfix

--- models/todo.go
+++ models/todo.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Todo struct {
-	Id      primitive.ObjectID `json:"id,omitempty"`
+	Id      primitive.ObjectID `bson:"_id" json:"id"`
 	Title   string             `json:"title,omitempty"`
 	Content string             `json:"content,omitempty"`
 }
